36c28a46a156731af788e88c226f929d462a504a
Lossfaith <lossfaith@mail.ru>
2021-02-25 13:31:29 +0300

    Start_work

--- main.tf
+++ main.tf
@@ -17,3 +17,39 @@ terraform {
 provider "aws" {
   region = var.region
 }
+data "data_availability_zones" "Zones" {}
+#--------------------------------------------------------
+resource "aws_security_group" "SerafimSecurityGroup" {
+  name                  = "SecurityGroup"
+  vpc_security_group_id = [aws_security_group.SerafimSecurityGroup.id]
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+#----------------------------------------------
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "my-vpc"
+  cidr = "172.16.0.0/16"
+
+  azs            = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  public_subnets = ["172.16.0.0/25", "172.16.1.0/24", "172.16.2.0/24"]
+
+}
+#----------------------------------------------
+resource "aws_alb" "A_Balancer" {
+  name               = "Balancer_Public"
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.SerafimSecurityGroup.id]
+  //subnets            = aws_subnet..id
+}
--- s3.tf
+++ s3.tf
@@ -1,9 +1,7 @@
-resource "aws_s3_bucket" "test"
-{
+resource "aws_s3_bucket" "test" {
   bucket_prefix = "artem"
 
-    tags =
-    {
-      Name = "Simple"
-    }
+  tags = {
+    Name = "Simple"
+  }
 }
