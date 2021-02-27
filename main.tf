2954de0fe9e48f08656f5ee60cb336f1de6142e4
Artsiom Sinitsa <artsiomasi@gmail.com>
2021-02-27 14:59:37 +0300

    fix listener

--- main.tf
+++ main.tf
@@ -57,26 +57,11 @@ module "vpc" {
 
 }
 #----------------------------------------------
-resource "aws_alb" "A_Balancer" {
+resource "aws_alb" "balancer" {
   name               = "Balancer_Public"
   load_balancer_type = "application"
   security_groups    = [aws_security_group.SerafimSecurityGroup.id]
   subnets            = module.vpc.public_subnets
-  listener{
-    lb_port = 80
-    lb_protocol = "http"
-    instance_port = 80
-    instance_protocol = "http"
-  }
-  /*
-  health_check{
-    healthy_threshold = 2
-    unhealthy_threshold = 2
-    timeout = 3
-    target = "HTTP:80/"
-    interval = 10
-  }
-  */
 }
 #-----------------------------------------------
 resource "aws_instance" "master" {
@@ -95,9 +80,25 @@ resource "aws_lb_target_group" "test" {
   protocol = "HTTP"
   vpc_id   = module.vpc.vpc_id
 }
+
+resource "aws_lb_listener" "front_end" {
+  load_balancer_arn = aws_alb.balancer.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "Fixed response content"
+      status_code  = "200"
+    }
+  }
+}
 #-----------------------------------------------
 resource "aws_lb_listener_rule" "static" {
-  listener_arn = aws_alb.A_Balancer.listener
+  listener_arn = aws_lb_listener.front_end.arn
   priority     = 100
 
   action {
