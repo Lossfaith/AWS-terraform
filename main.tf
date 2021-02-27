6967c736edba4a2373508bc1caf7d76ee585a2b8
Artsiom Sinitsa <artsiomasi@gmail.com>
2021-02-27 15:33:45 +0300

    target attachment

--- main.tf
+++ main.tf
@@ -58,7 +58,7 @@ resource "aws_security_group" "SerafimSecurityGroup" {
 }
 #----------------------------------------------
 resource "aws_alb" "balancer" {
-  name               = "Balancer_Public"
+  name               = "BalancerPublic"
   load_balancer_type = "application"
   security_groups    = [aws_security_group.SerafimSecurityGroup.id]
   subnets            = module.vpc.public_subnets
@@ -67,22 +67,23 @@ resource "aws_alb" "balancer" {
 resource "aws_instance" "master" {
   count = var.count_instances
 
-  ami                 = data.aws_ami.ubuntu
-  instance_type       = "t3.micro"
-  subnet_id           = module.vpc.public_subnets[count.index % length(module.vpc.public_subnets)]
-  security_groups     = [aws_security_group.SerafimSecurityGroup.id]
+  ami             = data.aws_ami.ubuntu
+  instance_type   = "t3.micro"
+  subnet_id       = module.vpc.public_subnets[count.index % length(module.vpc.public_subnets)]
+  security_groups = [aws_security_group.SerafimSecurityGroup.id]
 }
 #-----------------------------------------------
 resource "aws_lb_target_group" "test" {
-  name     = "tf-example-lb-tg"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = module.vpc.vpc_id
+  name        = "tf-example-lb-tg"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = module.vpc.vpc_id
   target_type = "instance"
 }
 resource "aws_lb_target_group_attachment" "test" {
+  count            = var.count_instances
   target_group_arn = aws_lb_target_group.test.arn
-  target_id        = aws_instance.master.id
+  target_id        = aws_instance.master[count.index].id
   port             = 80
 }
 resource "aws_lb_listener" "front_end" {
