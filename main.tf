bdf425da962cc82c004d27c87d45a824a89d6592
Artsiom Sinitsa <artsiomasi@gmail.com>
2021-03-13 20:48:09 +0300

    template

--- main.tf
+++ main.tf
@@ -56,60 +56,60 @@ resource "aws_security_group" "SerafimSecurityGroup" {
   }
 }
 
-//#----------------------------------------------
-//resource "aws_alb" "balancer" {
-//  name               = "BalancerPublic"
-//  load_balancer_type = "application"
-//  security_groups    = [aws_security_group.SerafimSecurityGroup.id]
-//  subnets            = module.vpc.public_subnets
-//}
-//#-----------------------------------------------
-//resource "aws_instance" "master" {
-//  count                  = var.count_instances
-//  vpc_security_group_ids = [aws_security_group.SerafimSecurityGroup.id]
-//  ami                    = data.aws_ami.ubuntu.id
-//  instance_type          = "t3.micro"
-//  subnet_id              = module.vpc.public_subnets[count.index % length(module.vpc.public_subnets)]
-//  user_data              = templatefile("${path.module}/user_data.sh.tmpl", {
-//  d_name = aws_route53_record.www.name
-//})
-//#-----------------------------------------------
-//resource "aws_lb_target_group" "test" {
-//  name        = "tf-example-lb-tg"
-//  port        = 80
-//  protocol    = "HTTP"
-//  vpc_id      = module.vpc.vpc_id
-//  target_type = "instance"
-//  health_check {
-//    enabled             = true
-//    path                = "/index.html"
-//    healthy_threshold   = 2
-//    unhealthy_threshold = 2
-//    timeout             = 5
-//    port                = 80
-//    interval            = 10
-//    protocol            = "HTTP"
-//  }
-//}
-//#-------------------------------------------------
-//resource "aws_lb_target_group_attachment" "test" {
-//  count            = var.count_instances
-//  target_group_arn = aws_lb_target_group.test.arn
-//  target_id        = aws_instance.master[count.index].id
-//  port             = 80
-//}
-//#--------------------------------------------------
-//resource "aws_lb_listener" "front_end" {
-//  load_balancer_arn = aws_alb.balancer.arn
-//  port              = "80"
-//  protocol          = "HTTP"
-//
-//  default_action {
-//    target_group_arn = aws_lb_target_group.test.id
-//    type             = "forward"
-//  }
-//}
-//#-----------------------------------------------
-//output "web_loadbalancer_url" {
-//  value = aws_alb.balancer.dns_name
-//}
+#----------------------------------------------
+resource "aws_alb" "balancer" {
+  name               = "BalancerPublic"
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.SerafimSecurityGroup.id]
+  subnets            = module.vpc.public_subnets
+}
+#-----------------------------------------------
+resource "aws_instance" "master" {
+  count                  = var.count_instances
+  vpc_security_group_ids = [aws_security_group.SerafimSecurityGroup.id]
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t3.micro"
+  subnet_id              = module.vpc.public_subnets[count.index % length(module.vpc.public_subnets)]
+  user_data              = templatefile("${path.module}/user_data.sh.tmpl", {
+  d_name = aws_route53_record.www.name)
+}
+#-----------------------------------------------
+resource "aws_lb_target_group" "test" {
+  name        = "tf-example-lb-tg"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = module.vpc.vpc_id
+  target_type = "instance"
+  health_check = {
+    enabled             = true
+    path                = "/index.html"
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 5
+    port                = 80
+    interval            = 10
+    protocol            = "HTTP"
+  }
+}
+#-------------------------------------------------
+resource "aws_lb_target_group_attachment" "test" {
+  count            = var.count_instances
+  target_group_arn = aws_lb_target_group.test.arn
+  target_id        = aws_instance.master[count.index].id
+  port             = 80
+}
+#--------------------------------------------------
+resource "aws_lb_listener" "front_end" {
+  load_balancer_arn = aws_alb.balancer.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action = {
+    target_group_arn = aws_lb_target_group
+    type             = "forward"
+  }
+}
+#-----------------------------------------------
+output "web_loadbalancer_url" {
+  value = aws_alb.balancer.dns_name
+}
