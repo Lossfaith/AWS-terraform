69c35cd7facbfb142e4f445a065fcb549536da4d
Lossfaith <lossfaith@mail.ru>
2021-03-16 16:54:53 +0300

    finish2

--- route53.tf
+++ route53.tf
@@ -5,11 +5,17 @@ data "aws_route53_zone" "external" {
 
 resource "aws_route53_record" "www" {
   zone_id = data.aws_route53_zone.external.zone_id
-  name    = "www.${data.aws_route53_zone.external.name}"
+  name    = ["www.${data.aws_route53_zone.external.name}", "${data.aws_route53_zone.external.name}"]
   type    = "A"
-  ttl     = "300"
-  records = ["10.0.0.1"]
+  //ttl     = "300"
+  //records = ["10.0.0.1"]
+  alias {
+    name                   = aws_alb.balancer.dns_name
+    zone_id                = aws_alb.balancer.zone_id
+    evaluate_target_health = true
+  }
 }
+
 
 output "external-www" {
   value = aws_route53_record.www.name
